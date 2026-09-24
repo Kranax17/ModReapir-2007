39b383f5c7df118b8752c236bdece15b7b13182b
Kranax <59462592+Kranax17@users.noreply.github.com>
2026-09-23 19:05:19 -0700

    Update config.py

--- tuberepair/config.py
+++ tuberepair/config.py
@@ -68,7 +68,7 @@ else:
 if "URL" in OSEnv:
     URL = OSEnv["URL"]
 else:
-    URL = "https://inv2.truehosting.net"
+    URL = "http://yewtu.be"
 
 # Max res to allow users
 # Mainly to stop converting long numbers to strings.
